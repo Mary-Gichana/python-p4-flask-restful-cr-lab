8bc10cd19937d3d874091292e8356a1c809ecf1a
Mary Monchari Gichana <marygichana95@gmail.com>
2025-01-19 19:53:51 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -17,11 +17,41 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        '''GET /plants - Return a list of all plants'''
+        plants = Plant.query.all()
+        response = [plant.to_dict() for plant in plants]
+        return make_response(jsonify(response), 200)
+
+    def post(self):
+        '''POST /plants - Create a new plant'''
+        data = request.get_json()
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price']
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response_dict = new_plant.to_dict()
+        return make_response(jsonify(response_dict), 201)
+
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        '''GET /plants/<int:id> - Return a specific plant by ID'''
+        plant = db.session.get(Plant, id)  
+        if plant is None:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+        response_dict = plant.to_dict()
+        return make_response(jsonify(response_dict), 200)
+
+
+
+# Add the resources to the API
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,9 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import DECIMAL
 from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
+   
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(255), nullable=True)  
+    price = db.Column(db.Float, nullable=True) 
